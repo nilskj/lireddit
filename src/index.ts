a74e6ab812f488e35be9d11a4ae000ec369be608
Nils Kjellman <nils.kjellman@stenaline.com>
2020-11-09 15:38:19 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { PostResolver } from "./resolvers/post";
+import "reflect-metadata";
+import { HelloResolver } from "./resolvers/hello";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import mikroConfig from "./mikro-orm-config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
+main().catch((err) => console.log(err));
